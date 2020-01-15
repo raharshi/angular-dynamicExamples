9f82323f126ec9f1b7c8736bc23d2d38a6ad046e
raharshi <raharshi.puli@gmail.com>
2020-01-15 14:48:34 +0530

    dynamic select added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { DynamicSelectionTableExampleComponent } from './dynamic-selection-table-example/dynamic-selection-table-example.component';
 import {MatCheckboxModule} from '@angular/material/checkbox';
@@ -11,6 +11,11 @@ import { MatIconModule } from '@angular/material/icon';
 import { AppService } from './app.service';
 import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 import { DynamicFormFieldsComponent } from './dynamic-form-fields/dynamic-form-fields.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import {MatSelectModule} from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from'@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -20,12 +25,19 @@ import { DynamicFormFieldsComponent } from './dynamic-form-fields/dynamic-form-f
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     MatTableModule,
     MatPaginatorModule,
     MatSortModule,
     MatCheckboxModule,
     MatIconModule,
     HttpClientModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule
   ],
   providers: [AppService],
   bootstrap: [AppComponent]
--- src/app/dynamic-form-fields/dynamic-form-fields.component.ts
+++ src/app/dynamic-form-fields/dynamic-form-fields.component.ts
@@ -6,7 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dynamic-form-fields.component.css']
 })
 export class DynamicFormFieldsComponent implements OnInit {
-
+  dropselect:any;
+  cars:any[]= [{
+          manufacturer: 'Porsche',
+          model: '911',
+          price: 135000,
+          wiki: 'http://en.wikipedia.org/wiki/Porsche_997',
+          img: 'https://upload.wikimedia.org/wikipedia/commons/c/c4/Porsche_997_Turbo_-_Flickr_-_Alexandre_Pr%C3%A9vot_%288%29.jpg'
+        },{
+            manufacturer: 'Nissan',
+            model: 'GT-R',
+            price: 80000,
+            wiki:'http://en.wikipedia.org/wiki/Nissan_Gt-r',
+            img: 'https://upload.wikimedia.org/wikipedia/commons/d/d1/2012_Nissan_GT-R_Egoist.jpg'
+        },{
+            manufacturer: 'BMW',
+            model: 'M3',
+            price: 60500,
+            wiki:'http://en.wikipedia.org/wiki/Bmw_m3',
+            img: 'https://upload.wikimedia.org/wikipedia/commons/f/f6/2018_BMW_M3_3.0.jpg'
+        },{
+            manufacturer: 'Audi',
+            model: 'S5',
+            price: 53000,
+            wiki:'http://en.wikipedia.org/wiki/Audi_S5#Audi_S5',
+            img: 'https://upload.wikimedia.org/wikipedia/commons/7/7d/2018_Audi_S5_TFSi_Quattro_Automatic_3.0_Front.jpg'
+        },{
+            manufacturer: 'Audi',
+            model: 'TT',
+            price: 40000,
+            wiki:'http://en.wikipedia.org/wiki/Audi_TT',
+            img: 'https://upload.wikimedia.org/wikipedia/commons/f/f5/Audi_TT_Coup%C3%A9_2.0_TFSI_quattro_S-line_%288S%29_%E2%80%93_Frontansicht%2C_3._April_2015%2C_D%C3%BCsseldorf.jpg'
+        }];
+  foods: any[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
   constructor() { }
 
   ngOnInit() {
